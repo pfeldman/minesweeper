4ae66f5171205313847fc35d09e1f233500f3cf1
Pablo Feldman <pablo.feldman@globant.com>
2016-01-11 11:00:07 -0300

    Fix issues

--- assets/js/client/minesweeper.js
+++ assets/js/client/minesweeper.js
@@ -1,211 +1,209 @@
 function Piece(number, mine, parent) {
-	this.number = number;
-	this.mine = mine;
-	this.open = false;
-	this.dom = undefined;
+    this.number = number;
+    this.mine = mine;
+    this.open = false;
+    this.dom = undefined;
 
-	this.render = function() {
-		this.dom = $('<div class="piece"></div>');
+    this.render = function() {
+        this.dom = $('<div class="piece"></div>');
 
-		var instance = this;
+        var instance = this;
 
-		this.dom.bind("contextmenu", function() {
-			instance.dom.toggleClass("flag");
-			return false;
-		});
+        this.dom.bind("contextmenu", function() {
+            instance.dom.toggleClass("flag");
+            return false;
+        });
 
-		this.dom.click(function(){
-			if(instance.hasMine())
-			{
-				parent.canWin = false;
-				alert("You have lose");
-				parent.showMines();
-				return;
-			}
+        this.dom.click(function(){
+            if(instance.hasMine())
+            {
+                parent.canWin = false;
+                alert("You have lose");
+                parent.showMines();
+                return;
+            }
 
-			var nearMines = parent.searchMines(instance.number);
-			if(nearMines > 0)
-			{
-				instance.dom.text(nearMines);
-				instance.dom.addClass("mines" + nearMines);
-			}
-			instance.dom.addClass("opened");
-			instance.show();
-		});
+            var nearMines = parent.searchMines(instance.number);
+            if(nearMines > 0)
+            {
+                instance.dom.text(nearMines);
+                instance.dom.addClass("mines" + nearMines);
+            }
+            instance.dom.addClass("opened");
+            instance.show();
+        });
 
-		return this.dom;
-	};
+        return this.dom;
+    };
 
-	this.show = function(){
-		if(this.hasMine())
-			this.dom.addClass("mine");
+    this.show = function(){
+        if(this.hasMine())
+            this.dom.addClass("mine");
 
-		this.open = true;
-		parent.addOpened();
-	};
+        this.open = true;
+        parent.addOpened();
+    };
 
-	this.isOpen = function() {
-		return this.open;
-	};
+    this.isOpen = function() {
+        return this.open;
+    };
 
-	this.hasMine = function() {
-		return this.mine;
-	};
+    this.hasMine = function() {
+        return this.mine;
+    };
 }
 
 function Board(settings, parentDom) {
-	this.mines = [];
-	this.settings = settings;
-	this.parentDom = parentDom;
-	this.board = [];
-	this.totalOpened = 0;
-	this.canWin = true;
+    this.mines = [];
+    this.settings = settings;
+    this.parentDom = parentDom;
+    this.board = [];
+    this.totalOpened = 0;
+    this.canWin = true;
 
-	this.addOpened = function() {
-		this.totalOpened++;
+    this.addOpened = function() {
+        this.totalOpened++;
 
-		if(this.settings.width * this.settings.height - this.totalOpened - this.settings.mines === 0)
-		{
-			this.win();
-		}
-	};
+        if(this.settings.width * this.settings.height - this.totalOpened - this.settings.mines === 0)
+        {
+            this.win();
+        }
+    };
 
-	this.win = function() {
-		if(this.canWin)
-		{
-			$(".overlay").show();
-			$(".shareOn").fadeIn("slow");
-			console.log($(".shareOn label"));
-			$(".shareOn label").click(function(){
-				location.reload();
-			});
-			$(".shareOnFacebook").click(function(){
-				var fbpopup = window.open("https://www.facebook.com/sharer/sharer.php?u=http://www.google.com", "pop", "width=600, height=400, scrollbars=no");
-			});
-		}
-	};
+    this.win = function() {
+        if(this.canWin)
+        {
+            $(".overlay").show();
+            $(".shareOn").fadeIn("slow");
+            $(".shareOn label").click(function(){
+                location.reload();
+            });
 
-	this.searchMines = function(number) {
-		var mines = 0;
-		var position = this.getPosition(number);
+            $(".shareOnFacebook").click(function(){
+                var fbpopup = window.open("https://www.facebook.com/sharer/sharer.php?u=http://www.google.com", "pop", "width=600, height=400, scrollbars=no");
+            });
+        }
+    };
 
-		if(this.board[position.x - 1] !== undefined)
-			if(this.board[position.x - 1][position.y].hasMine())
-				mines++;
+    this.searchMines = function(number) {
+        var mines = 0;
+        var position = this.getPosition(number);
 
-		if(this.board[position.x + 1] !== undefined)
-			if(this.board[position.x + 1][position.y].hasMine())
-				mines++;
+        if(this.board[position.x - 1] !== undefined)
+            if(this.board[position.x - 1][position.y].hasMine())
+                mines++;
 
-		if(this.board[position.x][position.y - 1] !== undefined)
-			if(this.board[position.x][position.y - 1].hasMine())
-				mines++;
+        if(this.board[position.x + 1] !== undefined)
+            if(this.board[position.x + 1][position.y].hasMine())
+                mines++;
 
-		if(this.board[position.x][position.y + 1] !== undefined)
-			if(this.board[position.x][position.y + 1].hasMine())
-				mines++;
+        if(this.board[position.x][position.y - 1] !== undefined)
+            if(this.board[position.x][position.y - 1].hasMine())
+                mines++;
 
-		if(this.board[position.x - 1] !== undefined && this.board[position.x - 1][position.y - 1] !== undefined)
-			if(this.board[position.x - 1][position.y - 1].hasMine())
-				mines++;
+        if(this.board[position.x][position.y + 1] !== undefined)
+            if(this.board[position.x][position.y + 1].hasMine())
+                mines++;
 
-		if(this.board[position.x - 1] !== undefined && this.board[position.x - 1][position.y + 1] !== undefined)
-			if(this.board[position.x - 1][position.y + 1].hasMine())
-				mines++;
+        if(this.board[position.x - 1] !== undefined && this.board[position.x - 1][position.y - 1] !== undefined)
+            if(this.board[position.x - 1][position.y - 1].hasMine())
+                mines++;
 
-		if(this.board[position.x + 1] !== undefined && this.board[position.x + 1][position.y + 1] !== undefined)
-			if(this.board[position.x + 1][position.y + 1].hasMine())
-				mines++;
+        if(this.board[position.x - 1] !== undefined && this.board[position.x - 1][position.y + 1] !== undefined)
+            if(this.board[position.x - 1][position.y + 1].hasMine())
+                mines++;
 
-		if(this.board[position.x + 1] !== undefined && this.board[position.x + 1][position.y - 1] !== undefined)
-			if(this.board[position.x + 1][position.y - 1].hasMine())
-				mines++;
+        if(this.board[position.x + 1] !== undefined && this.board[position.x + 1][position.y + 1] !== undefined)
+            if(this.board[position.x + 1][position.y + 1].hasMine())
+                mines++;
 
-		return mines;
-	};
+        if(this.board[position.x + 1] !== undefined && this.board[position.x + 1][position.y - 1] !== undefined)
+            if(this.board[position.x + 1][position.y - 1].hasMine())
+                mines++;
 
-	this.showMines = function() {
-		for(var mine in this.mines)
-		{
-			var position = this.getPosition(this.mines[mine]);
-			this.board[position.x][position.y].show();
-		}
-	};
+        return mines;
+    };
 
-	this.getPosition = function(position) {
-		var y = Math.floor(position / this.settings.width);
-		var x = Math.floor(parseFloat((position / this.settings.width) - y).toPrecision(12) * this.settings.width);
+    this.showMines = function() {
+        for(var mine in this.mines)
+        {
+            var position = this.getPosition(this.mines[mine]);
+            this.board[position.x][position.y].show();
+        }
+    };
 
-		return {'x' : x, 'y': y};
-	};
+    this.getPosition = function(position) {
+        var y = Math.floor(position / this.settings.width);
+        var x = Math.floor(parseFloat((position / this.settings.width) - y).toPrecision(12) * this.settings.width);
 
-	this.renderBoard = function() {
-		var i;
-		var boardArray = [];
+        return {'x' : x, 'y': y};
+    };
 
-		for(i = 0; i < this.settings.width * this.settings.height; i++)
-		{
-			boardArray.push(i + 1);
-		}
+    this.renderBoard = function() {
+        var i;
+        var boardArray = [];
 
-		for(i=0; i < this.settings.mines; i++)
-		{
-			var random = boardArray[Math.floor(Math.random()*boardArray.length)];
-			this.mines.push(random);
-			boardArray.splice(random-1, 1);
-		}
+        for(i = 0; i < this.settings.width * this.settings.height; i++)
+        {
+            boardArray.push(i + 1);
+        }
 
-		var pieces = 0;
-		var newLine = true;
-		var appendToDom;
-		for(i = 0; i < this.settings.width * this.settings.height; i++)
-		{
-			var lineCreated;
-			if(newLine)
-			{
-				lineCreated = $('<div class="line"></div>');
-				this.parentDom.append(lineCreated);
-				newLine = false;
-			}
+        for(i=0; i < this.settings.mines; i++)
+        {
+            var random = boardArray[Math.floor(Math.random()*boardArray.length)];
+            this.mines.push(random);
+            boardArray.splice(random-1, 1);
+        }
 
-			var haveMine = false;
+        var pieces = 0;
+        var newLine = true;
+        var appendToDom;
+        for(i = 0; i < this.settings.width * this.settings.height; i++)
+        {
+            var lineCreated;
+            if(newLine)
+            {
+                lineCreated = $('<div class="line"></div>');
+                this.parentDom.append(lineCreated);
+                newLine = false;
+            }
 
-			if(this.mines.indexOf(i) >= 0)
-				haveMine = true;
+            var haveMine = false;
 
-			var piece = new Piece(i, haveMine, this);
+            if(this.mines.indexOf(i) >= 0)
+                haveMine = true;
 
-			var position = this.getPosition(i);
+            var piece = new Piece(i, haveMine, this);
 
+            var position = this.getPosition(i);
 
+            if(this.board[position.x] === undefined)
+                this.board[position.x] = [];
 
-			if(this.board[position.x] === undefined)
-				this.board[position.x] = [];
+            this.board[position.x].push(piece);
 
-			this.board[position.x].push(piece);
+            lineCreated.append(piece.render());
+            pieces++;
 
-			lineCreated.append(piece.render());
-			pieces++;
+            if(pieces == this.settings.width)
+            {
+                pieces = 0;
+                newLine = true;
+            }
+        }
 
-			if(pieces == this.settings.width)
-			{
-				pieces = 0;
-				newLine = true;
-			}
-		}
-
-		this.parentDom.css({
-			'margin-left': this.settings.width * -25
-		});
-	};
+        this.parentDom.css({
+            'margin-left': this.settings.width * -25
+        });
+    };
 }
 
 $(function(){
-	var board = new Board({
-		width : 10,
-		height : 10,
-		mines : 10
-	}, $(".minesweeperBoard"));
+    var board = new Board({
+        width : 10,
+        height : 10,
+        mines : 10
+    }, $(".minesweeperBoard"));
 
-	board.renderBoard();
+    board.renderBoard();
 });
